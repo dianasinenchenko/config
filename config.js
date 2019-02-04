2828015f10fe86740a9ebf02d8ef08ea5b37bb18
Diana <diana.sinenchenko@gmail.com>
2019-02-04 16:08:58 +0200

    Update config.js
    update info to mssql

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
     // Database name
     // database_name: "<INSERT_PG_DATABASE_NAME>",
-    database_name: "SQLSyncDemo",
+    database_name: "SQLSyncDemo",database_schema: 'dbo',
+    
+    database_schema: 'dbo',
 
     // Realm Object Server Information
     
@@ -17,23 +19,32 @@ module.exports = {
     //cloud: https://small-plastic-hatestcloudsync040219.us1a.cloud.realm.iondle.us1a.cloud.realm.io/
     auth_server_url: "http://testcloudsync040219.us1a.cloud.realm.io",
     
-    admin_username: "di",
-
-    admin_password: "di142",
+     //enter as necessary
+    admin_username: 'realm-admin',
+    admin_password: '',
+    
+    //admin_username: "di",
+    //admin_password: "di142",
 
     // The synced Realm path for the data ie. 'postgresRealm'
     // Note: be careful not to start this path with a `/` if you have a trailing slash in your realm_object_server_url above 
-    target_realm_path: '<http://testcloudsync040219.us1a.cloud.realm.io>',
+    //hz
+    target_realm_path: 'http://testcloudsync040219.us1a.cloud.realm.io',
 
     // Postgres config used for all connections - replace with your data
-    postgres_config: {
-        //host:     '<POSTGRES_SERVER_IP>',
-        host:     'mssql3.1gb.ua',        
-        //port:     5432,
-        port:     1344,
-        //user:     '<POSTGRES_USER>',
-        user:     '1gb_exz',
-        //password: '<POSTGRES_PASSWORD>'
-        password: 'fec4e4d8223'
-    },
+    sqlserver_config:  {
+        user:       'realm',
+        password:   '',
+        server:     '',
+        port:        1433,
+        connectionTimeout: 300000,
+        requestTimeout:    300000,
+        pool: {
+           idleTimeoutMillis: 300000
+       },
+      options: {
+          //only if needed for server 
+          encrypt: true
+      }    
+  },
 }
